9788bbf6c78ce707a8f8374a6e2aad5f752b9217
manojs15 <manojshivappa.153@gmail.com>
2024-03-05 20:29:12 +0530

    adding styles to login dashboard

--- farmer/farmer/settings.py
+++ farmer/farmer/settings.py
@@ -130,3 +130,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media"
+
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
