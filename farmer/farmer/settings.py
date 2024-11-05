a714a4fa3aa3622aa213c2a319549f266f6ca7ad
kavipriya2004 <kavipriya.kousalya2004@gmail.com>
2024-11-05 15:42:59 +0530

    initial commit

--- farmer/farmer/settings.py
+++ farmer/farmer/settings.py
@@ -145,7 +145,7 @@ MEDIA_ROOT = BASE_DIR / "media"
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [BASE_DIR / 'farmer' / 'static']
 
 LOGIN_REDIRECT_URL = 'app:dashboard'
 
